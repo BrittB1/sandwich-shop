51d6338440d0c76af54f2c2b7bbf8a3451edbcae
Brittany Buchanan <bbuchanan0400@gmail.com>
2024-11-13 11:29:35 -0500

    Created sandwich size enum with descriptions

---
+++ capstone-2-deli/src/main/java/com/pluralsight/deli/SandwichSize.java
@@ -0,0 +1,28 @@
+package com.pluralsight.deli;
+
+public enum SandwichSize {
+
+    SMALL("4-inch for when you're just a little hungry", 4),
+    MEDIUM("8-inch that's great to share, if you want to", 8),
+    LARGE("12-inch for those times when you're so hungry you could eat a horse", 12);
+
+    /* describes size, shows the user what they'll be getting */
+
+
+    private String description;
+    private int inches;
+
+    SandwichSize(String description, int inches) {
+        this.description = description;
+        this.inches = inches;
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getInches() {
+        return inches;
+    }
+    }
