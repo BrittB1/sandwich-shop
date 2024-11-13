4fc3e3babf8e9b5e47c20a76c9e3691a1146e8ae
Brittany Buchanan <bbuchanan0400@gmail.com>
2024-11-13 16:46:58 -0500

    Created methods that ask user for sandwich size, bread type, and toppings choices

--- capstone-2-deli/src/main/java/com/pluralsight/deli/OrderScreen.java
+++ capstone-2-deli/src/main/java/com/pluralsight/deli/OrderScreen.java
@@ -1,20 +1,20 @@
 package com.pluralsight.deli;
 
-
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class OrderScreen {
 
     private final Order order;
-
+Scanner userInput = null;
     public OrderScreen(Order order) {
         this.order = order;
 
     }
 
     public void startOrder(Scanner input) {
-
-        while(true) {
+  userInput = input;
+        while (true) {
             System.out.println("""
                     
                         =============================
@@ -24,7 +24,7 @@ public class OrderScreen {
                             A. Add Sandwich
                             D. Add Drink
                             C. Add Chips
-                            T. Checkout
+                            O. Checkout
                             X. Cancel Order
                     """);
             // create switch case and call add sandwich method
@@ -34,9 +34,16 @@ public class OrderScreen {
 
                 case "A":
 // todo: create a sandwich and add it too the order
+                    RegularBread type = askUserForBreadType();
+                    SandwichSize size = askUserForSize();
+                    RegularToppings toppings = askUserForToppings();
+                    Sandwich s = new Sandwich(size);
+                    order.addSandwich(s);
+
                     break;
                 case "D":
                     // todo: create a drink and add it to the order
+
                     break;
 
                 case "C":
@@ -56,5 +63,52 @@ public class OrderScreen {
             }
 
         }
-}
+    }
+
+    private RegularToppings askUserForToppings() {
+            System.out.println("What toppings would you like?");
+            RegularToppings [] toppings = RegularToppings.values();
+
+            int i = 1;
+            for (RegularToppings topping: toppings ){
+                System.out.printf("%d. %s\n",i++,topping);
+
+            }
+            String inputString = userInput.nextLine();
+            Integer choice = Integer.parseInt(inputString);
+
+            return toppings[choice - 1] ;
+
+    }
+
+    private RegularBread askUserForBreadType() {
+        System.out.println("What type of bread do you want?");
+        RegularBread [] types = RegularBread.values();
+
+        int i = 1;
+        for (RegularBread type: types ){
+            System.out.printf("%d. %s\n",i++,type);
+
+        }
+        String inputString = userInput.nextLine();
+        Integer choice = Integer.parseInt(inputString);
+
+        return types[choice - 1] ;
+    }
+
+
+    private SandwichSize askUserForSize() {
+      System.out.println("What size do you want?");
+        SandwichSize [] sizes = SandwichSize.values();
+
+        int i = 1;
+        for (SandwichSize size: sizes ){
+            System.out.printf("%d. %s\n",i++,size);
+
+        }
+       String inputString = userInput.nextLine();
+        Integer choice = Integer.parseInt(inputString);
+
+        return sizes[choice - 1] ;
+    }
 }
