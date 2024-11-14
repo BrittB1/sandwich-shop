d834c2979835ad601df2eb1b8ea300a414118f47
Brittany Buchanan <bbuchanan0400@gmail.com>
2024-11-14 10:43:21 -0500

    Provided methods that displays drink + size choices and allows user to select choices using an integer.

---
+++ capstone-2-deli/src/main/java/com/pluralsight/deli/Beverages.java
@@ -0,0 +1,4 @@
+package com.pluralsight.deli;
+
+public enum Beverages { COKE, CHERRY_COKE, PINEAPPLE_FANTA, SPRITE, DR_PEPPER
+}
--- capstone-2-deli/src/main/java/com/pluralsight/deli/OrderScreen.java
+++ capstone-2-deli/src/main/java/com/pluralsight/deli/OrderScreen.java
@@ -1,19 +1,20 @@
 package com.pluralsight.deli;
 
-import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class OrderScreen {
 
     private final Order order;
-Scanner userInput = null;
+    Scanner userInput = null;
+
     public OrderScreen(Order order) {
         this.order = order;
 
     }
 
     public void startOrder(Scanner input) {
-  userInput = input;
+        userInput = input;
         while (true) {
             System.out.println("""
                     
@@ -43,7 +44,8 @@ Scanner userInput = null;
                     break;
                 case "D":
                     // todo: create a drink and add it to the order
-
+                    Beverages flavors = askUserForDrinkFlavor();
+                    SodaSize cup = askUserForDrinkSize();
                     break;
 
                 case "C":
@@ -58,57 +60,85 @@ Scanner userInput = null;
                     return;
 
                 default:
-
-
             }
-
         }
     }
 
-    private RegularToppings askUserForToppings() {
-            System.out.println("What toppings would you like?");
-            RegularToppings [] toppings = RegularToppings.values();
 
-            int i = 1;
-            for (RegularToppings topping: toppings ){
-                System.out.printf("%d. %s\n",i++,topping);
-
-            }
-            String inputString = userInput.nextLine();
-            Integer choice = Integer.parseInt(inputString);
-
-            return toppings[choice - 1] ;
-
-    }
-
-    private RegularBread askUserForBreadType() {
-        System.out.println("What type of bread do you want?");
-        RegularBread [] types = RegularBread.values();
+    private Beverages askUserForDrinkFlavor() {
+        System.out.println("What kind of drink would you like? ");
+        Beverages[] flavors = Beverages.values();
 
         int i = 1;
-        for (RegularBread type: types ){
-            System.out.printf("%d. %s\n",i++,type);
+        for (Beverages flavor : flavors) {
+            System.out.printf("%d. %s\n", i++, flavor);
 
         }
         String inputString = userInput.nextLine();
         Integer choice = Integer.parseInt(inputString);
 
-        return types[choice - 1] ;
+        return flavors[choice - 1];
+    }
+
+    private SodaSize askUserForDrinkSize() {
+        System.out.println("What size would you like for your drink?");
+        SodaSize[] cups = SodaSize.values();
+
+        int i = 1;
+        for (SodaSize cup : cups) ;
+        {
+            System.out.printf("%d. %s\n", i++, Arrays.toString(cups));
+        }
+        String inputString = userInput.nextLine();
+        Integer choice = Integer.parseInt(inputString);
+
+        return cups[choice - 1];
+    }
+
+    private RegularToppings askUserForToppings() {
+        System.out.println("What toppings would you like?");
+        RegularToppings[] toppings = RegularToppings.values();
+
+        int i = 1;
+        for (RegularToppings topping : toppings) {
+            System.out.printf("%d. %s\n", i++, topping);
+
+        }
+        String inputString = userInput.nextLine();
+        Integer choice = Integer.parseInt(inputString);
+
+        return toppings[choice - 1];
+
+    }
+
+    private RegularBread askUserForBreadType() {
+        System.out.println("What type of bread do you want?");
+        RegularBread[] types = RegularBread.values();
+
+        int i = 1;
+        for (RegularBread type : types) {
+            System.out.printf("%d. %s\n", i++, type);
+
+        }
+        String inputString = userInput.nextLine();
+        Integer choice = Integer.parseInt(inputString);
+
+        return types[choice - 1];
     }
 
 
     private SandwichSize askUserForSize() {
-      System.out.println("What size do you want?");
-        SandwichSize [] sizes = SandwichSize.values();
+        System.out.println("What size do you want?");
+        SandwichSize[] sizes = SandwichSize.values();
 
         int i = 1;
-        for (SandwichSize size: sizes ){
-            System.out.printf("%d. %s\n",i++,size);
+        for (SandwichSize size : sizes) {
+            System.out.printf("%d. %s\n", i++, size);
 
         }
-       String inputString = userInput.nextLine();
+        String inputString = userInput.nextLine();
         Integer choice = Integer.parseInt(inputString);
 
-        return sizes[choice - 1] ;
+        return sizes[choice - 1];
     }
 }
